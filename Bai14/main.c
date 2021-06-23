fd72e6292a09294fd15c5d2ab3ab3f3e2d177c1b
pdchinh <pdc1761.n@gmail.com>
2021-06-23 19:16:15 +0700

    ss4 :))

--- Bai14/main.c
+++ Bai14/main.c
@@ -35,10 +35,42 @@ int mul(int a, int k, int n)
 	return b;
 }
 
+
+int dkCanVaDu(int n)
+{
+	int N =n;
+	int size = 0;
+	int i = 2;
+	int before =1;
+	while(n>1)
+	{
+		if(n%i==0)
+		{
+			if(i!=before)
+			{
+				size++;
+				before = i;
+				if((N-1)%(before-1)!=0)
+				{
+					return 0;
+				}
+			}else{
+				return 0;
+			}
+			n/=i;
+		}else{
+			i++;
+		}
+	}
+	if(size >=3)
+	{
+		return 1;
+	}
+}
+
 int isCarmichael(int n)
 {
 	int hopSo = 0;
-	int flag = 0;
 	for(int i = 2; i<=n-2;++i)
 	{
 		if(n%i==0)
@@ -47,18 +79,16 @@ int isCarmichael(int n)
 		}
 		if(gcd(n,i)==1)
 		{
-			flag = 1;
 			if(mul(i, n -1, n)!=1)
 				return 0;
 		}
 	}
-	if(hopSo&&flag)
+	if(hopSo&&dkCanVaDu(n))
 	{
 		return 1;
 	}
 	return 0;
 }
-
 
 
 int main()
